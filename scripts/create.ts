ab4d6db68d3579f77a023ea534c27c237f8f3993
Luca De Santis <lucaroma2705@gmail.com>
2023-05-16 22:45:34 +0200

    Feat: add script for create component folder

---
+++ scripts/create.ts
@@ -0,0 +1,65 @@
+import prompts from "prompts";
+import fs from "fs";
+import path from "path";
+
+function capitalize(str: string) {
+  return str[0].toUpperCase() + str.slice(1);
+}
+
+const errMessage = (name: string, folder: string) =>
+  `Component '${capitalize(name)}' already exists in folder '${folder}'`;
+
+function isValidComponent(name: string, folder: string) {
+  console.log(name, folder);
+  const componentsPath = path.resolve("./src/", folder, name);
+  return !fs.existsSync(componentsPath);
+}
+
+const { folder } = await prompts({
+  type: "select",
+  name: "folder",
+  message: "Component folder?",
+  choices: [
+    { title: "components", value: "components", selected: true },
+    { title: "routes", value: "routes" },
+  ],
+});
+
+const { componentName } = await prompts({
+  type: "text",
+  name: "componentName",
+  message: "Component name?",
+  format: (name) => capitalize(name),
+  validate: (name) =>
+    isValidComponent(capitalize(name), folder) || errMessage(name, folder),
+});
+
+const INDEX_FILE = [
+  `export ${
+    folder === "components" ? "*" : `{ default as ${componentName} }`
+  } from "./${componentName}"`,
+  folder === "components"
+    ? `export type { ${componentName}Props } from "./${componentName}"`
+    : null,
+].join("\n");
+
+const INTERFACE = [`export interface ${componentName}Props {`, "", "}"].join(
+  "\n"
+);
+
+const COMPONENT_FILE = [
+  folder === "components" ? INTERFACE : null,
+  `function ${componentName}() {`,
+  `  return <div>${componentName}</div>`,
+  "}",
+  "",
+  `export default ${componentName}`,
+].join("\n");
+
+const componentPath = path.resolve("./src/", folder, componentName);
+fs.mkdirSync(componentPath);
+fs.writeFileSync(path.resolve(componentPath, "index.ts"), INDEX_FILE);
+fs.writeFileSync(
+  path.resolve(componentPath, `${componentName}.tsx`),
+  COMPONENT_FILE
+);
