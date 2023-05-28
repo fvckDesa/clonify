de8997e7d89f22fb183847380d9e41b8a460814b
Luca De Santis <lucaroma2705@gmail.com>
2023-05-28 15:38:23 +0200

    Feat: create loader fn in routes

--- scripts/create.ts
+++ scripts/create.ts
@@ -10,7 +10,6 @@ const errMessage = (name: string, folder: string) =>
   `Component '${capitalize(name)}' already exists in folder '${folder}'`;
 
 function isValidComponent(name: string, folder: string) {
-  console.log(name, folder);
   const componentsPath = path.resolve("./src/", folder, name);
   return !fs.existsSync(componentsPath);
 }
@@ -47,27 +46,42 @@ const { componentName } = await prompts(
 const INDEX_FILE = [
   `export ${
     folder === "components" ? "{ default }" : `{ default as ${componentName} }`
-  } from "./${componentName}"`,
+  } from "./${componentName}";`,
   folder === "components"
-    ? `export type { ${componentName}Props } from "./${componentName}"`
-    : null,
+    ? `export type { ${componentName}Props } from "./${componentName}";`
+    : `export { loader as loader${componentName} } from "./loader";`,
 ].join("\n");
 
 const INTERFACE = [`export interface ${componentName}Props {`, "", "}"].join(
   "\n"
 );
+const FN_PROPS = `{}: ${componentName}Props`;
 
 const COMPONENT_FILE = [
   folder === "components" ? INTERFACE : null,
-  `function ${componentName}() {`,
-  `  return <div>${componentName}</div>`,
+  "",
+  `function ${componentName}(${folder === "components" ? FN_PROPS : ""}) {`,
+  `  return <div>${componentName}</div>;`,
   "}",
   "",
-  `export default ${componentName}`,
+  `export default ${componentName};`,
+].join("\n");
+
+const LOADER_FILE = [
+  'import { LoaderFunctionArgs } from "react-router-dom";',
+  "",
+  "export function loader({}: LoaderFunctionArgs) {",
+  "",
+  "}",
 ].join("\n");
 
 const componentPath = path.resolve("./src/", folder, componentName);
 fs.mkdirSync(componentPath);
+
+if (folder === "routes") {
+  fs.writeFileSync(path.resolve(componentPath, "loader.ts"), LOADER_FILE);
+}
+
 fs.writeFileSync(path.resolve(componentPath, "index.ts"), INDEX_FILE);
 fs.writeFileSync(
   path.resolve(componentPath, `${componentName}.tsx`),
