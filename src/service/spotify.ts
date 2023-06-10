6c8947f2d6d0f58a165528a169a297c91204b45e
Luca De Santis <lucaroma2705@gmail.com>
2023-06-10 16:44:23 +0200

    Feat: refresh token
    Automatic request for new access token when it expired

--- src/service/spotify.ts
+++ src/service/spotify.ts
@@ -1,5 +1,5 @@
 const BASE_API_URL = "https://api.spotify.com/v1";
-const ACCESS_TOKEN_URL = "https://accounts.spotify.com/api/token";
+const TOKEN_URL = "https://accounts.spotify.com/api/token";
 
 interface AccessTokenInfo {
   access_token: string;
@@ -11,11 +11,49 @@ interface AccessTokenInfo {
 
 class SpotifyApi {
   private token: AccessTokenInfo | null = null;
+  private expired = 0;
 
   public constructor() {
     this.token = JSON.parse(
       sessionStorage.getItem("token") ?? "null"
     ) as AccessTokenInfo | null;
+    this.expired = +(sessionStorage.getItem("expired") ?? "0");
+  }
+
+  private setToken(token: AccessTokenInfo) {
+    this.token = token;
+    this.expired = Date.now() + this.token.expires_in;
+
+    sessionStorage.setItem("token", JSON.stringify(this.token));
+    sessionStorage.setItem("expired", String(this.expired));
+  }
+
+  private async refresh(): Promise<void> {
+    if (!this.token) {
+      throw new Error("Refresh token not found");
+    }
+
+    const res = await fetch(TOKEN_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        client_id: import.meta.env.VITE_SPOTIFY_ID,
+        client_secret: import.meta.env.VITE_SPOTIFY_SECRET,
+        grant_type: "refresh_token",
+        refresh_token: this.token.refresh_token,
+      }),
+    });
+
+    if (!res.ok) {
+      console.log(await res.json());
+      throw new Error(`Refresh token request failed! Status: ${res.status}`);
+    }
+
+    const newToken = (await res.json()) as AccessTokenInfo;
+
+    this.setToken({ ...this.token, ...newToken });
   }
 
   public async request(
@@ -31,14 +69,29 @@ class SpotifyApi {
     const headers = new Headers(request.headers);
     headers.set("Authorization", `${token_type} ${access_token}`);
 
-    return fetch(`${BASE_API_URL}/${path}`, {
+    const res = await fetch(`${BASE_API_URL}/${path}`, {
       ...request,
       headers,
     });
+
+    if (!res.ok) {
+      const { error } = (await res.json()) as {
+        error: { status: number; message: string };
+      };
+
+      if (error.status === 401 && this.expired < Date.now()) {
+        await this.refresh();
+        return this.request(path, request);
+      }
+
+      throw error;
+    }
+
+    return res;
   }
 
   public async setAccessToken(code: string): Promise<void> {
-    const res = await fetch(ACCESS_TOKEN_URL, {
+    const res = await fetch(TOKEN_URL, {
       method: "POST",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
@@ -57,9 +110,7 @@ class SpotifyApi {
       throw new Error(`Access token request failed! Status: ${res.status}`);
     }
 
-    this.token = (await res.json()) as AccessTokenInfo;
-
-    sessionStorage.setItem("token", JSON.stringify(this.token));
+    this.setToken((await res.json()) as AccessTokenInfo);
   }
 
   public get authUrl() {
