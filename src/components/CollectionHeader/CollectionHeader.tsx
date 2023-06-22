ce2f58d24a47439b0dedd1a3ad47ceae9b3fcb34
Luca De Santis <lucaroma2705@gmail.com>
2023-06-23 00:58:00 +0200

    Refact: generalize

--- src/components/CollectionHeader/CollectionHeader.tsx
+++ src/components/CollectionHeader/CollectionHeader.tsx
@@ -1,62 +1,22 @@
 import styled from "styled-components";
-import { time, format } from "@utils/time";
-import { useMemo } from "react";
-import Authors, { Separator } from "@components/Authors";
-import { PartialArtist } from "@/types/spotify";
-
+import { ReactNode } from "react";
 export interface CollectionHeaderProps {
-  title: string;
-  type: string;
-  authors: PartialArtist[];
   cover: string;
-  tracksNum: number;
-  duration: number;
+  size: number;
+  className?: string;
+  children?: ReactNode | ReactNode[];
 }
 
 function CollectionHeader({
-  title,
-  type,
-  authors,
   cover,
-  tracksNum,
-  duration,
+  size,
+  className,
+  children,
 }: CollectionHeaderProps) {
-  const durationStr = useMemo(() => {
-    const timeObj = time(duration);
-    const timeParts: string[] = [];
-
-    if (timeObj.hours > 0) {
-      timeParts.push("{h} hr");
-    }
-    if (timeObj.minutes > 0) {
-      timeParts.push("{m} min");
-    }
-    if (timeObj.seconds > 0 && timeObj.hours === 0) {
-      timeParts.push("{s} sec");
-    }
-
-    return format(timeParts.join(" "), timeObj);
-  }, [duration]);
-
-  const _type = useMemo(
-    () => (type === "single" && tracksNum > 1 ? "EP" : type),
-    [type, tracksNum]
-  );
-
   return (
-    <Header>
-      <Cover src={cover} />
-      <TextContainer>
-        <h3 className="type">{_type}</h3>
-        <h1 className="title">{title}</h1>
-        <Info>
-          <Authors authors={authors} separator={{ content: "•", space: 5 }} />
-          <Separator $content="•" $space={0} />
-          <span>{tracksNum} tracks</span>
-          {","}
-          <span className="duration">{durationStr}</span>
-        </Info>
-      </TextContainer>
+    <Header className={className}>
+      <Cover src={cover} $size={size} />
+      <TextContainer>{children}</TextContainer>
     </Header>
   );
 }
@@ -64,15 +24,12 @@ function CollectionHeader({
 const Header = styled.header`
   display: flex;
   gap: 24px;
-  height: clamp(340px, 30vh, 400px);
-  padding: 24px 16px;
-  padding-top: 0;
 `;
 
-const Cover = styled.img`
+const Cover = styled.img<{ $size: number }>`
   align-self: flex-end;
-  width: 192px;
-  height: 192px;
+  width: ${(props) => `${props.$size}px`};
+  height: ${(props) => `${props.$size}px`};
   background-color: ${({ theme }) => theme.colors.primary};
 `;
 
@@ -82,25 +39,7 @@ const TextContainer = styled.div`
   flex-direction: column;
   justify-content: flex-end;
   gap: 8px;
-  height: 100%;
-  & > .type {
-    font-size: 0.875rem;
-    text-transform: capitalize;
-  }
-  & > .title {
-    font-size: 4.5rem;
-  }
-`;
-
-const Info = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 4px;
-  font-size: 0.875rem;
-  font-weight: 700;
-  & > .duration {
-    color: ${({ theme }) => theme.colors.grayText};
-  }
+  height: auto;
 `;
 
 export default CollectionHeader;
